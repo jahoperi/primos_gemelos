d175af7f07de906c4d0355753fbad23b8b119875
jahoperi <97061564+jahoperi@users.noreply.github.com>
2022-04-06 17:57:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,16 +8,11 @@ Created on Sun Mar 20 20:25:47 2022
 import streamlit as st
 import base64
 
-st.title("Prueba de la Conjetura de los números primos gemelos")
-
 st.subheader("")
-
-st.subheader("jahoperi@gmail.com")
-
 def show_pdf(file_path):
     with open(file_path,"rb") as f:
           base64_pdf = base64.b64encode(f.read()).decode('utf-8')
     pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
-show_pdf("papaer-1.pdf")
+show_pdf("CV.pdf")
